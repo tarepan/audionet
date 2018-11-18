60d3b87b7d4f16b084d968d1540fb5506c2b1cc9
Tarepan <tarepan5884@gmail.com>
2018-11-18 17:26:10 +0900

    Add library components

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 setup(
     name="audionet",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires = [
+        "numpy",
         "functional",
         "librosa",
         "pyworld"
